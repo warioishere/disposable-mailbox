9072624eb399d7839827f4c48ab9865ba50653fe
wario_is_here <117512622+warioishere@users.noreply.github.com>
2024-12-25 22:19:15 +0100

    Update User.php to use latest php features

--- src/User.php
+++ src/User.php
@@ -1,9 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 class User {
-    public $address;
-    public $username;
-    public $domain;
+    public string $address;
+    public string $username;
+    public string $domain;
 
     public static function get_random_address(array $domains): string {
         $wordLength = rand(3, 8);
@@ -18,63 +20,46 @@ class User {
     }
 
     public function isInvalid(array $config_domains): bool {
-        if (empty($this->username) || empty($this->domain)) {
-            return true;
-        } elseif (!in_array($this->domain, $config_domains)) {
-            return true;
-        } else {
-            return false;
-        }
+        return empty($this->username) || empty($this->domain) || !in_array($this->domain, $config_domains, true);
     }
 
     public static function parseDomain(string $address, array $config_blocked_usernames): User {
-        $clean_address = User::_clean_address($address);
-        $user = new User();
-        $user->username = User::_clean_username($clean_address, $config_blocked_usernames);
-        $user->domain = User::_clean_domain($clean_address);
+        $clean_address = self::_clean_address($address);
+        $user = new self();
+        $user->username = self::_clean_username($clean_address, $config_blocked_usernames);
+        $user->domain = self::_clean_domain($clean_address);
         $user->address = $user->username . '@' . $user->domain;
         return $user;
     }
 
-    public static function parseUsernameAndDomain(string $username, string $domain, $config_blocked_usernames): User {
-        $user = new User();
-        $user->username = User::_clean_username($username, $config_blocked_usernames);
-        $user->domain = User::_clean_domain($domain);
+    public static function parseUsernameAndDomain(string $username, string $domain, array $config_blocked_usernames): User {
+        $user = new self();
+        $user->username = self::_clean_username($username, $config_blocked_usernames);
+        $user->domain = self::_clean_domain($domain);
         $user->address = $user->username . '@' . $user->domain;
         return $user;
     }
 
-    /**
-     * Remove illegal characters from address.
-     * @return string clean address
-     */
-    private static function _clean_address(string $address) {
+    private static function _clean_address(string $address): string {
         return strtolower(filter_var($address, FILTER_SANITIZE_EMAIL));
     }
 
-
-    /**
-     * Remove illegal characters from username and remove everything after the @-sign. You may extend it if your server supports them.
-     *
-     * @return string clean username
-     */
-    private static function _clean_username(string $address, array $config_blocked_usernames) {
+    private static function _clean_username(string $address, array $config_blocked_usernames): string {
         $username = strtolower($address);
-        $username = preg_replace('/@.*$/', "", $username);   // remove part after @
-        $username = preg_replace('/[^A-Za-z0-9_.+-]/', "", $username);   // remove special characters
+        $username = preg_replace('/@.*$/', '', $username);  // Entfernt den Teil nach @
+        $username = preg_replace('/[^A-Za-z0-9_.+-]/', '', $username);  // Entfernt unerwünschte Zeichen
 
-        if (in_array($username, $config_blocked_usernames)) {
-            // Forbidden name!
+        if (in_array($username, $config_blocked_usernames, true)) {
+            // Verbotener Benutzername
             return '';
         }
 
         return $username;
     }
 
-
-    private static function _clean_domain(string $address) {
-        $username = strtolower($address);
-        $username = preg_replace('/^.*@/', "", $username);   // remove part before @
-        return preg_replace('/[^A-Za-z0-9_.+-]/', "", $username);   // remove special characters
+    private static function _clean_domain(string $address): string {
+        $domain = strtolower($address);
+        $domain = preg_replace('/^.*@/', '', $domain);  // Entfernt den Teil vor @
+        return preg_replace('/[^A-Za-z0-9_.+-]/', '', $domain);  // Entfernt unerwünschte Zeichen
     }
 }
